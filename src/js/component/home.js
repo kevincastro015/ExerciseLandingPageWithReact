e8201a0ab0b50133356793f21ac7cb9b5bf66e4f
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-27 18:58:34 +0000

    firstcommit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,64 @@
 import React from "react";
-
+import Navbar from "./navbar";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+    let array = [
+    {
+        title: "Title for card 1"
+        description: "Sentence for card 1"
+        image: "url for card 1", 
+        goTo: ""
+    }
+     {
+        title: "Title for card 1"
+        description: "Sentence for card 1"
+        image: "url for card 1", 
+        goTo: ""
+    } {
+        title: "Title for card 1"
+        description: "Sentence for card 1"
+        image: "url for card 1", 
+        goTo: ""
+    }
+     {
+        title: "Title for card 1"
+        description: "Sentence for card 1"
+        image: "url for card 1", 
+        goTo: ""
+    }
+];
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+            <Navbar />
+            <Jumbotron />
+            <Card />
+            <Footer />
+			<h1>A Warm Welcome</h1>
+            {array.map((element) => {
+                return (
+                <span 
+                key={element.title}
+                className="card" 
+                style={{width: "18rem"}}>
+                <img 
+                    className="card-img-top" 
+                    src={element.image} 
+                    alt="Card image cap"
+                />
+                <div className="card-body">
+                    <h5 className="card-title">{element.title}</h5>
+                    <p className="card-text">{element.description}</p>
+                    <a href="" className="btn btn-primary">
+                        Go somewhere
+                        </a>
+                </div>
+            </span>
+            );
+        })}
+    </div>
+    );
 }
