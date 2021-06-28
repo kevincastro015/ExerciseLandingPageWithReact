9aba5bf1f087af13c34bc6a6096a42834708cec4
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-28 21:53:35 +0000

    mustdebug

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
@@ -36,8 +36,8 @@ export function Home() {
 
 	return (
 		<div className="text-center mt-5">
-			<Navbar />
-			<Jumbotron />
+			<Navbar />,
+            <Jumbotron/>,
 
 			{array.map(element => {
 				return (
